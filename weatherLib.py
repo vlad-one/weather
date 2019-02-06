d5e6c871210bc7726c579037a186e5d20e6c645c
vlad <vladimir@example.com>
2019-02-05 20:43:53 -0500

    init version

---
+++ weatherLib.py
@@ -0,0 +1,221 @@
+import SimpleHTTPServer
+import SocketServer
+from thread import start_new_thread
+import time
+import glob
+import urllib2
+import re
+import json
+
+class weatherNOAA():
+    city="City"
+    sky="sky"
+    
+    def __init__(self):
+        self.State="ny"
+        self.Zone="nyz003"
+        self.City="Rochester"
+        self.dateRegEx = re.compile("[0-9]{3,4}\s+[AP]M.+\s[0-9]{4}")
+        self.urlCurrent = "http://tgftp.nws.noaa.gov/data/observations/state_roundup/{}/{}.txt".format(self.State,self.Zone)
+        self.urlZone = "http://tgftp.nws.noaa.gov/data/forecasts/zone/{}/{}.txt".format(self.State,self.Zone)
+        self.urlState = "http://tgftp.nws.noaa.gov/data/forecasts/state/{}/{}.txt".format(self.State,self.Zone)
+        
+        return
+        
+#         self.tmp = current["temp"]
+#         for var in current:
+#             setattr(self, var, current[var])
+    
+    def fetchData(self,fromURL) :
+        print "Reading", fromURL
+        req = urllib2.Request(fromURL)
+        req.add_header('Cache-Control', 'max-age=0')
+        resp = urllib2.urlopen(req)
+        content = resp.read()
+        resp.close()
+        return content.split("\n")
+
+
+    def getCurrent(self):
+        
+        content = self.fetchData(self.urlCurrent)
+
+        """
+http://tgftp.nws.noaa.gov/data/observations/metar/stations/KROC.TXT
+http://tgftp.nws.noaa.gov/data/observations/metar/decoded/KROC.TXT
+
+
+        2018/10/09 15:54
+        KROC 091554Z 24014G20KT 10SM FEW045 FEW150 SCT250 28/20 A3018 RMK AO2 SLP219 T02830200 $
+        """
+    
+        """
+    CITY           SKY/WX    TMP DP  RH WIND       PRES   REMARKS
+    ROCHESTER      CLOUDY    54  54 100 E5        30.35F FOG
+        """
+         
+    #     reCurrentWeather = re.compile( r"(?P<city>ROCHESTER)\s+"
+    #                                     "(?P<sky>\w+)\s+"
+    #                                     "(?P<tmp>\w+)"
+    #                                     "(?P<dp>\w+)"
+    #                                     "(?P<rh>\w+)"
+    #                                     "(?P<wind>\w+)"
+    #                                     ".*")
+        
+    #    posName = {"city":0,"sky":15,"tmp":25,"dp":29,"rh":32,"wind":36,"pres":46,"rem":53,"last":100}
+    #              CITY           SKY/WX    TMP DP  RH WIND       PRES   REMARKS
+        pos =    ( 0,             15,       25, 29, 32,36,        46,    53,   100)
+        pNames = ("city",        "sky",    "temp","dp","rh","wind","pres","rem","last")
+         
+        dateTimeLine = 6 
+        dateTime = "None"
+         
+        currentLineN = 0
+    
+    #    print content
+                
+        regionData = []
+        
+        for ln in content :
+            if currentLineN == dateTimeLine :
+                dateTime = ln.strip()
+            
+            currentLineN +=1
+    #        m = reCurrentWeather.match(ln)
+            rec = {}
+            s = 0
+            e = 0
+            for i in range (1,len(pos)) :
+                e = pos[i]
+                rec[pNames[i-1]]=ln[s:e].strip()
+                s = e
+            if rec["city"].startswith(self.City.upper()) :
+                regionData = rec
+                break
+        if "temp" in regionData :
+            tF = float(regionData["temp"])
+            tC = round((tF - 32) * 10 /9 , 0 ) / 2
+            regionData["temp"] = "{:.1f}C / {:.0f}F".format(tC,tF)
+            regionData["time"] = dateTime 
+        return regionData
+
+
+    def forecasts_week(self):
+        """
+445 PM EST Fri Nov 30 2018
+
+   FCST     FCST     FCST     FCST     FCST     FCST     FCST     
+   Sat      Sun      Mon      Tue      Wed      Thu      Fri      
+   Dec 01   Dec 02   Dec 03   Dec 04   Dec 05   Dec 06   Dec 07   
+
+   Rochester, NY
+   Rain     Rain     Snoshwr  Mocldy   Mocldy   Mocldy   Mocldy   
+   30/37    37/57    42/42    27/32    26/32    25/32    25/33    
+   10/60   100/60    50/70    40/30    30/20    20/20    40/40   
+        """
+
+        stateA = self.fetchData(self.urlState)
+
+        prnt = 0
+        headFound = False
+        state = [[],[],[],[],[],[],[]]
+
+        capTime = "N/A"
+
+        for v in stateA:
+            s = v.strip()
+        #    print "{}".format(v.strip())
+
+            m = self.dateRegEx.match(v)
+            if m:
+                capTime=v
+                continue
+
+            if s.find("FCST") >= 0:
+                prnt = 2
+                headFound = True
+                continue
+
+            if s.find(self.City) >= 0 and headFound:
+                prnt = 3
+                continue
+
+            if prnt :
+                a=[] 
+                for i in range(0,7) :
+                    j=i*9
+                    state[i].append(s[j:j+9].strip())
+                prnt -= 1
+
+        return {"time":capTime,"data":state}
+
+
+#############################
+    def forecasts_zone(self):
+        """
+http://tgftp.nws.noaa.gov/data/forecasts/zone/ny/nyz003.txt
+
+930 PM EST Fri Nov 30 2018
+
+.OVERNIGHT...Cloudy with some patchy fog. Lows around 30. Light
+winds. 
+.SATURDAY...Some patchy fog in the morning, otherwise cloudy. Rain
+likely in the afternoon. Highs in the upper 30s. Light winds,
+becoming east 5 to 10 mph. Chance of rain 60 percent. 
+        """
+
+        zone = self.fetchData(self.urlZone)
+
+        zoneA = []
+        #zoneKey = -1
+
+        addNextLine = False
+
+        for ln in zone :
+            if len(ln) < 2 :
+                addNextLine = False
+                continue
+            
+            m = self.dateRegEx.match(ln)
+            if m:
+#                zoneA[0] = ln
+                Time=ln
+                continue
+            
+            if ln[0] == ".":
+                e = ln.find("...")
+                if e > 0:
+                    lnA = ln[1:].replace("...","#",1)
+                    zoneA.append(lnA.split("#"))
+                    addNextLine = True
+                continue    
+
+            if addNextLine :
+                zoneA[-1][1] += " " + ln
+                continue
+        # TODO error checking
+        return {"time":Time,"data":zoneA}
+
+#######################################
+
+    """
+http://tgftp.nws.noaa.gov/data/forecasts/nowcast/ny/nyz003.txt - outdated
+
+
+    """
+
+
+if __name__ == "__main__" :
+
+    W = weatherNOAA()
+
+    current = W.getCurrent()
+
+    print current["time"]
+
+    print json.dumps(current)
+
+    print json.dumps(W.forecasts_week())
+
+    print json.dumps(W.forecasts_zone())
+
